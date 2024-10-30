d0ef5a868dd141d5a9118ef7f24c6291aa092e25
Zhifeng Wang <51321751+zf-w@users.noreply.github.com>
2024-10-30 10:47:57 -0500

    [Content] adjust style and clean up 'prelude.js'.

--- src/prelude.js
+++ src/prelude.js
@@ -19,26 +19,21 @@
  * ```
  */
 
-const ROOT_ELEM_ID = "$root";
-const MENU_ICON_ELEM_ID = "$menu_icon";
+function pre_content_load_procedure() {
+  const PAGE_ROOT_ELEM_ID = "$root";
 
-const SIDEBAR_STORAGE_KEY = "sidebar";
-const SIDEBAR_ON_CLASS = "sidebar_on";
-const THEME_KEY = "theme";
+  const LOCAL_STORAGE_SIDEBAR_KEY = "sidebar";
+  const LOCAL_STORAGE_THEME_KEY = "theme";
 
-const html_elem_mut_ref = document.getElementById(ROOT_ELEM_ID);
+  const SIDEBAR_ON_CLASS = "sidebar_on";
 
-const THEME_STRINGS_LIST = ["theme_zf_navy", "theme_zf_honey"];
-let curr_theme_string = THEME_STRINGS_LIST[0];
+  const THEME_STRINGS_LIST = ["theme_zf_navy", "theme_zf_honey"];
 
-function switch_theme(to_theme_string) {
-  localStorage.setItem(THEME_KEY, to_theme_string);
-  html_elem_mut_ref.classList.replace(curr_theme_string, to_theme_string);
-  curr_theme_string = to_theme_string;
-}
+  const html_elem_mut_ref = document.getElementById(PAGE_ROOT_ELEM_ID);
 
-if (html_elem_mut_ref != undefined) {
-  let stored_theme_string = localStorage.getItem(THEME_KEY);
+  let curr_theme_string = THEME_STRINGS_LIST[0];
+
+  const stored_theme_string = localStorage.getItem(LOCAL_STORAGE_THEME_KEY);
   if (
     stored_theme_string != undefined &&
     THEME_STRINGS_LIST.find((v) => {
@@ -47,71 +42,99 @@ if (html_elem_mut_ref != undefined) {
   ) {
     curr_theme_string = stored_theme_string;
   }
-  html_elem_mut_ref.classList.toggle(curr_theme_string);
 
-  const sidebar_storage = localStorage.getItem(SIDEBAR_STORAGE_KEY);
-  if (sidebar_storage == null) {
-    if (window.innerWidth > 800) {
+  const sidebar_storage = localStorage.getItem(LOCAL_STORAGE_SIDEBAR_KEY);
+  if (html_elem_mut_ref != undefined) {
+    html_elem_mut_ref.classList.toggle(curr_theme_string);
+
+    if (sidebar_storage == null) {
+      if (window.innerWidth > 800) {
+        html_elem_mut_ref.classList.toggle(SIDEBAR_ON_CLASS);
+      }
+    } else if (sidebar_storage == "on") {
       html_elem_mut_ref.classList.toggle(SIDEBAR_ON_CLASS);
     }
-  } else if (sidebar_storage == "on") {
-    html_elem_mut_ref.classList.toggle(SIDEBAR_ON_CLASS);
   }
-}
 
-window.mdtome = { fetched_jsons: {} };
+  window.mdtome = { fetched_jsons: {}, curr_theme_string };
 
-window.mdtome.fetch_static_json_helper = (url) => {
-  let entry = window.mdtome.fetched_jsons[url];
-  if (entry == undefined) {
-    window.mdtome.fetched_jsons[url] = {};
-    entry = window.mdtome.fetched_jsons[url];
-  }
-  if (entry.data != undefined) {
-    console.log(`Fetch cached ${url}`);
-    return { data: entry.data };
-  } else {
-    if (entry.wait == undefined) {
-      entry.wait = [];
-
-      fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-          entry.data = data;
-          for (let i = 0; i < entry.wait.length; ++i) {
-            const promise_ref = entry.wait[i];
-            console.log(`Resolving promise ${i} on ${url}`);
-            promise_ref.resolve(data);
-          }
-        })
-        .catch((err_msg) => {
-          for (let i = 0; i < entry.wait.length; ++i) {
-            const promise_ref = entry.wait[i];
-            promise_ref.reject(err_msg);
-          }
-        });
+  window.mdtome.fetch_static_json_helper = (url) => {
+    let entry = window.mdtome.fetched_jsons[url];
+    if (entry == undefined) {
+      window.mdtome.fetched_jsons[url] = {};
+      entry = window.mdtome.fetched_jsons[url];
     }
-    return {
-      promise: new Promise((resolve, reject) => {
-        entry.wait.push({ resolve, reject });
-      }),
-    };
-  }
-};
+    if (entry.data != undefined) {
+      console.log(`Fetch cached ${url}`);
+      return { data: entry.data };
+    } else {
+      if (entry.wait == undefined) {
+        entry.wait = [];
+
+        fetch(url)
+          .then((res) => res.json())
+          .then((data) => {
+            entry.data = data;
+            for (let i = 0; i < entry.wait.length; ++i) {
+              const promise_ref = entry.wait[i];
+              console.log(`Resolving promise ${i} on ${url}`);
+              promise_ref.resolve(data);
+            }
+          })
+          .catch((err_msg) => {
+            for (let i = 0; i < entry.wait.length; ++i) {
+              const promise_ref = entry.wait[i];
+              promise_ref.reject(err_msg);
+            }
+          });
+      }
+      return {
+        promise: new Promise((resolve, reject) => {
+          entry.wait.push({ resolve, reject });
+        }),
+      };
+    }
+  };
+}
 
 function post_content_load_procedure() {
   const LISTENER_CLICK_EVT_STRING = "click";
-  const menu_icon_elem = document.getElementById(MENU_ICON_ELEM_ID);
+
+  const LOCAL_STORAGE_SIDEBAR_KEY = "sidebar";
+  const LOCAL_STORAGE_THEME_KEY = "theme";
+
+  const PAGE_LANG_ZH_STRING = "zh";
+  const PAGE_END_TIMESTAMP_ELEM_ID = "$end_timestamp";
+  const PAGE_MENU_ICON_ELEM_ID = "$menu_icon";
+  const PAGE_ROOT_ELEM_ID = "$root";
+
+  const THEME_STRINGS_LIST = ["theme_zf_navy", "theme_zf_honey"];
+
+  const SIDEBAR_ON_CLASS = "sidebar_on";
+
+  const html_elem_mut_ref = document.getElementById(PAGE_ROOT_ELEM_ID);
+
+  const menu_icon_elem = document.getElementById(PAGE_MENU_ICON_ELEM_ID);
+
   menu_icon_elem.addEventListener(LISTENER_CLICK_EVT_STRING, () => {
     if (html_elem_mut_ref) {
       if (html_elem_mut_ref.classList.contains(SIDEBAR_ON_CLASS)) {
-        localStorage.setItem(SIDEBAR_STORAGE_KEY, "off");
+        localStorage.setItem(LOCAL_STORAGE_SIDEBAR_KEY, "off");
       } else {
-        localStorage.setItem(SIDEBAR_STORAGE_KEY, "on");
+        localStorage.setItem(LOCAL_STORAGE_SIDEBAR_KEY, "on");
       }
       html_elem_mut_ref.classList.toggle(SIDEBAR_ON_CLASS);
     }
   });
+
+  function switch_theme(to_theme_string) {
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, to_theme_string);
+    html_elem_mut_ref.classList.replace(
+      window.mdtome.curr_theme_string,
+      to_theme_string
+    );
+    window.mdtome.curr_theme_string = to_theme_string;
+  }
 
   THEME_STRINGS_LIST.forEach((theme_class_string) => {
     const curr_theme_btn = document.getElementById(
@@ -123,4 +146,23 @@ function post_content_load_procedure() {
       });
     }
   });
+
+  const end_timestamp_elem_mut_ref = document.getElementById(
+    PAGE_END_TIMESTAMP_ELEM_ID
+  );
+  if (end_timestamp_elem_mut_ref != undefined) {
+    let update_msg_string = "Last Update";
+    if (
+      html_elem_mut_ref != undefined &&
+      html_elem_mut_ref.lang == PAGE_LANG_ZH_STRING
+    ) {
+      update_msg_string = "最后更新";
+    }
+    const date_string = new Date(
+      Number.parseInt(end_timestamp_elem_mut_ref.innerText)
+    ).toString();
+    end_timestamp_elem_mut_ref.innerText = `${update_msg_string}: ${date_string}`;
+  }
 }
+
+pre_content_load_procedure();
